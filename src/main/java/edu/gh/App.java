f2ec770be03902faaeaf1940d7e9b834787a8ee4
Greg Kotsiuba <hryhorii.kotsiuba@heg.com>
2017-04-08 17:10:25 +0300

    add skeleton

---
+++ src/main/java/edu/gh/App.java
@@ -0,0 +1,18 @@
+package edu.gh;
+
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App  {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    @Test
+    public void doS() {
+        System.out.println("123");
+    }
+}
